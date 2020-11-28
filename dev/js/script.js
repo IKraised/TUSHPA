aaaf580196418ffbda7f9b4395ab0a5945184cec
IKraised <Ikraised@yandex.ru>
2020-11-28 16:56:13 +0300

    start

--- dev/js/script.js
+++ dev/js/script.js
@@ -2,34 +2,27 @@
 
 $(document).ready(function () {
 
-    // sliders
+    // Слайдер
     $('[data-slider="items"]').each(function (index, slider) {
         $(slider).find('[data-sliders]').slick({
             slidesToShow: +$(slider).data('count'),
             slidesToScroll: 1,
             arrows: false,
             variableWidth: true,
-            // prevArrow: $(slider).find('[data-arrow="back"]'),
-            // nextArrow: $(slider).find('[data-arrow="next"]'),
-            responsive: [
-                {
-                  breakpoint: 400,
-                  settings: {
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                  }
-                }
-              ]
+           
         });
 
     });
-    $(document).find('.product-card__cart').on('click', function(e) {
+
+    // Показ выбора кол-во на карточке
+    $(document).on('click', '.product-card__cart', function(e) {
         e.preventDefault();
         $(this).find('.product-card__basket_ico').addClass('open');
         $(this).find('.product-card__counter').addClass('open');
     });
-    $('.product-card__counter-minus').click(function () {
+
+     // Счетчик кол-во минус
+    $('.product-card__counter-minus').on('click', function () {
         var $input = $(this).parent().find('input');
         var count = parseInt($input.val()) - 1;
         count = count < 1 ? 1 : count;
@@ -37,10 +30,24 @@ $(document).ready(function () {
         $input.change();
         return false;
     });
-    $('.product-card__counter-plus').click(function () {
+
+    // Счетчик кол-во плюс
+    $('.product-card__counter-plus').on('click', function () {
         var $input = $(this).parent().find('input');
         $input.val(parseInt($input.val()) + 1);
         $input.change();
         return false;
     });
+
+    // Выпадающий поиск
+    $(document).on('keyup', '.header__search-input input', function(e){
+
+        e.preventDefault();
+        if($(e.target).val().length > 0){
+            $('.search-bar').fadeIn();
+        }else{
+            $('.search-bar').fadeOut();
+        }
+
+    })    
 });
